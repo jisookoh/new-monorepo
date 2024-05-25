59c60b779a7fa26932cb0c875a3385add2b3c58c
고지수 <gosjioo0312@naver.com>
2024-05-25 20:05:28 +0900

    Feat: 로컬 데이터베이스 연결 완료

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      ConfigModule.forRoot({
+          isGlobal: true
+      }),
+      TypeOrmModule.forRootAsync({
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => ({
+              retryAttempts: configService.get('NODE_ENV') === 'prod' ? 10 : 1,
+              type: 'mysql',
+              host: configService.get('DB_HOST'),
+              port: Number(configService.get('DB_PORT')),
+              database: configService.get('DB_DATABASE'),
+              username: configService.get('DB_USER'),
+              password: configService.get('DB_PASSWORD'),
+              entities: [
+                  path.join(__dirname, 'src/entities/**/*.entity.{js, ts}'),
+              ],
+              synchronize: false,
+              logging: true,
+
+          }),
+      }),
+  ],
+  controllers: [],
+  providers: [],
+
 })
 export class AppModule {}
