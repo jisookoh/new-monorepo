36da7e19e26f644791b8de800cb4e65b1ec4739e
고지수 <gosjioo0312@naver.com>
2024-05-31 03:27:10 +0900

    Feat: Swagger 라이브러리 설치 및  config 추가

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ======= Swapper config =======
+  const config = new DocumentBuilder()
+      .setTitle("Board API")
+      .setDescription("Board REST API")
+      .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // ======= Swapper config End. =======
+
   await app.listen(8080);
 }
 bootstrap();
