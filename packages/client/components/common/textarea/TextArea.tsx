73101dbcebe6948e1245b5ac44be96ff0ab8d15c
고지수 <gosjioo0312@naver.com>
2024-06-25 04:41:26 +0900

    Feat: TextArea 컴포넌트 생성

---
+++ packages/client/components/common/textarea/TextArea.tsx
@@ -0,0 +1,28 @@
+import { InputHTMLAttributes, memo } from "react";
+import styled from "@emotion/styled";
+import {Colors} from "@/styles";
+
+interface TextAreaProps extends InputHTMLAttributes<HTMLTextAreaElement> {}
+
+const TextArea = ({
+                      ...rest
+                  }: TextAreaProps) => {
+    return (
+        <S.TextAreaContainer
+            {...rest}
+        />
+    )
+};
+
+export default memo(TextArea);
+
+namespace S {
+    export const TextAreaContainer = styled.textarea`
+        width: 100%;
+        height: 360px;
+        border: 1px solid ${Colors.Grey300};
+        padding: 16px;
+        resize: none;
+        border-radius: 8px;
+    `;
+}
